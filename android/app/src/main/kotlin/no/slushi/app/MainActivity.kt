04c10a98c1a6bfd9db077916025bb09b951863b2
Magne Noraberg <magne@Magnes-MBP.lan>
2026-03-01 20:48:49 +0100

    Release iOS build 16 (pin anchor fixed, UI cleanup)

--- android/app/src/main/kotlin/no/slushi/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package no.slushi.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/no/slushi/slushi/MainActivity.kt
@@ -0,0 +1,6 @@
+package no.slushi.slushi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
